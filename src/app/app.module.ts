ec5d24c616673487a3294adf47d6b6c377bb1f61
giaat <guillaume.peria@gmail.com>
2017-12-05 17:37:31 +0100

    Build our first code-driven form with all the Angular goodies.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
-import { ROUTES } from './app.routes';
+import { ReactiveFormsModule } from '@angular/forms';
 
+import { ROUTES } from './app.routes';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { RacesComponent } from './races/races.component';
@@ -12,6 +13,7 @@ import { RaceComponent } from './race/race.component';
 import { PonyComponent } from './pony/pony.component';
 import { FromNowPipe } from './from-now.pipe';
 import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
@@ -22,8 +24,9 @@ import { HomeComponent } from './home/home.component';
     PonyComponent,
     FromNowPipe,
     HomeComponent,
+    RegisterComponent,
   ],
-  imports: [BrowserModule, HttpClientModule, RouterModule.forRoot(ROUTES)],
+  imports: [BrowserModule, HttpClientModule, RouterModule.forRoot(ROUTES), ReactiveFormsModule],
   providers: [RaceService],
   bootstrap: [AppComponent],
 })
