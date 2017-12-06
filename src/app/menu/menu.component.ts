9152331fd8b0908a8cce2b6442e51d28b686e1af
giaat <guillaume.peria@gmail.com>
2017-12-06 15:58:05 +0100

    Display our logged in user and its score with the help of Observables and Subjects.

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
+
+import { UserService } from '../user.service';
+import { UserModel } from '../models/user.model';
 
 @Component({
   selector: 'pr-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css'],
 })
-export class MenuComponent implements OnInit {
+export class MenuComponent implements OnInit, OnDestroy {
   navbarCollapsed = true;
-  constructor() {}
+  user: UserModel;
+  userEventsSubscription: Subscription;
 
-  ngOnInit() {}
+  constructor(private userService: UserService) {}
+
+  ngOnInit() {
+    this.userEventsSubscription = this.userService.userEvents.subscribe(user => (this.user = user));
+  }
+
+  ngOnDestroy() {
+    if (this.userEventsSubscription) {
+      this.userEventsSubscription.unsubscribe();
+    }
+  }
 
   toggleNavbar() {
     this.navbarCollapsed = !this.navbarCollapsed;
--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,18 +1,25 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
+import { Subject } from 'rxjs/Subject';
+import 'rxjs/add/operator/do';
 import { HttpClient } from '@angular/common/http';
+import { UserModel } from './models/user.model';
 
 @Injectable()
 export class UserService {
   baseUrl = 'http://ponyracer.ninja-squad.com';
+  userEvents = new Subject<UserModel>();
+
   constructor(private http: HttpClient) {}
 
-  register(login, password, birthYear): Observable<any> {
+  register(login, password, birthYear): Observable<UserModel> {
     const body = { login, password, birthYear };
-    return this.http.post(`${this.baseUrl}/api/users`, body);
+    return this.http.post<UserModel>(`${this.baseUrl}/api/users`, body);
   }
-  authenticate(crendentials): Observable<any> {
-    const body = crendentials;
-    return this.http.post(`${this.baseUrl}/api/users/authentication`, body);
+
+  authenticate(crendentials): Observable<UserModel> {
+    return this.http
+      .post<UserModel>(`${this.baseUrl}/api/users/authentication`, crendentials)
+      .do(user => this.userEvents.next(user));
   }
 }
